bb029020a3fb5a2bd762b2109e6e23d2f198d88e
Rahul Pramod Jaguste <rahuljaguste@gmail.com>
2023-02-06 16:41:35 +0530

    revert build changes

--- stader/build.sh
+++ stader/build.sh
@@ -3,11 +3,11 @@
 export CGO_ENABLED=1
 echo $PWD
 
-GOOS=darwin GOARCH=amd64 go build -o stader-cli-darwin-amd64 stader-cli.go
+# GOOS=darwin GOARCH=amd64 go build -o stader-cli-darwin-amd64 stader-cli.go
 
 #
 ## Build x64 version
-#CGO_CFLAGS="-O -D__BLST_PORTABLE__" GOARCH=amd64 GOOS=mac go build -o stader-daemon-linux-amd64 stader.go
+CGO_CFLAGS="-O -D__BLST_PORTABLE__" GOARCH=amd64 GOOS=mac go build -o stader-daemon-linux-amd64 stader.go
 #
 ## Build the arm64 version
-#CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-cpp CGO_CFLAGS="-O -D__BLST_PORTABLE__" GOARCH=arm64 GOOS=linux go build -o stader-daemon-linux-arm64 stader.go
+CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-cpp CGO_CFLAGS="-O -D__BLST_PORTABLE__" GOARCH=arm64 GOOS=linux go build -o stader-daemon-linux-arm64 stader.go
