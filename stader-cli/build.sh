ffacee673027d689ad7114aa29f8c15dfdc46dfc
Bharath Vedartham <vedabharath12345@gmail.com>
2023-02-07 11:08:18 +0530

    Fix the build.sh file

--- stader-cli/build.sh
+++ stader-cli/build.sh
@@ -4,9 +4,9 @@ export CGO_ENABLED=0
 cd /stader-node/stader-cli
 
 # Build x64 version
-#GOOS=linux GOARCH=amd64 go build -o stader-cli-linux-amd64 stader-cli.go
-GOOS=darwin GOARCH=amd64 go build -o stader-cli-darwin-amd64 stader-cli.go
+GOOS=linux GOARCH=amd64 go build -o stader-cli-linux-amd64 stader-cli.go
+# GOOS=darwin GOARCH=amd64 go build -o stader-cli-darwin-amd64 stader-cli.go
 
 # Build the arm64 version
-#GOOS=linux GOARCH=arm64 go build -o stader-cli-linux-arm64 stader-cli.go
-GOOS=darwin GOARCH=arm64 go build -o stader-cli-darwin-arm64 stader-cli.go
+GOOS=linux GOARCH=arm64 go build -o stader-cli-linux-arm64 stader-cli.go
+# GOOS=darwin GOARCH=arm64 go build -o stader-cli-darwin-arm64 stader-cli.go
