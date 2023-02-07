a08be11cf0ee1102fa978a1fe4a98dac107ae909
Bharath Vedartham <vedabharath12345@gmail.com>
2023-02-07 15:17:10 +0530

    Add some debug logs

--- shared/services/services.go
+++ shared/services/services.go
@@ -107,7 +107,9 @@ func GetEthxPermisionlessPoolManager(c *cli.Context) (*stader.EthxContractManage
 		return nil, err
 	}
 
-	return stader.NewEthxPermisionlessPool(ec, common.BytesToAddress([]byte("0x2E9D0b1b10380DDB45DDc28B57B4A72E2d6b2782")))
+	ethPermisionlessPoolAddr := common.BytesToAddress([]byte("0x2E9D0b1b10380DDB45DDc28B57B4A72E2d6b2782"))
+	fmt.Printf("eth permisionless pool address is [%s]", ethPermisionlessPoolAddr)
+	return stader.NewEthxPermisionlessPool(ec, ethPermisionlessPoolAddr)
 }
 
 func GetRocketPool(c *cli.Context) (*rocketpool.RocketPool, error) {
