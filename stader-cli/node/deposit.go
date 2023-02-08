11f16ee098f6c557ffad57a18fbde97df9888192
Bharath Vedartham <vedabharath12345@gmail.com>
2023-02-08 18:52:59 +0530

    Reduce gas limit

--- stader-cli/node/deposit.go
+++ stader-cli/node/deposit.go
@@ -242,8 +242,8 @@ func nodeDeposit(c *cli.Context) error {
 
 	// Assign max fees
 	err = gas.AssignMaxFeeAndLimit(rocketpool.GasInfo{
-		EstGasLimit:  10000,
-		SafeGasLimit: 10000,
+		EstGasLimit:  1000,
+		SafeGasLimit: 1000,
 	}, staderClient, c.Bool("yes"))
 	if err != nil {
 		return err
