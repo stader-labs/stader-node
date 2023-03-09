f7b9a154d58c9b58d332432802c06ffadf7ae53d
Bharath Vedartham <vedabharath12345@gmail.com>
2023-03-09 14:51:20 +0530

    some more refactoring

--- stader/api/network/commands.go
+++ stader/api/network/commands.go
@@ -33,26 +33,6 @@ func RegisterSubcommands(command *cli.Command, name string, aliases []string) {
 
 				},
 			},
-
-			{
-				Name:      "rpl-price",
-				Aliases:   []string{"p"},
-				Usage:     "Get the current network RPL price in ETH",
-				UsageText: "stader-cli api network rpl-price",
-				Action: func(c *cli.Context) error {
-
-					// Validate args
-					if err := cliutils.ValidateArgCount(c, 0); err != nil {
-						return err
-					}
-
-					// Run
-					api.PrintResponse(getRplPrice(c))
-					return nil
-
-				},
-			},
-
 			{
 				Name:      "stats",
 				Aliases:   []string{"s"},
@@ -67,71 +47,6 @@ func RegisterSubcommands(command *cli.Command, name string, aliases []string) {
 
 					// Run
 					api.PrintResponse(getStats(c))
-					return nil
-
-				},
-			},
-
-			{
-				Name:      "timezone-map",
-				Aliases:   []string{"t"},
-				Usage:     "Get the table of node operators by timezone",
-				UsageText: "stader-cli api network stats",
-				Action: func(c *cli.Context) error {
-
-					// Validate args
-					if err := cliutils.ValidateArgCount(c, 0); err != nil {
-						return err
-					}
-
-					// Run
-					api.PrintResponse(getTimezones(c))
-					return nil
-
-				},
-			},
-
-			{
-				Name:      "can-generate-rewards-tree",
-				Usage:     "Check if the rewards tree for the provided interval can be generated",
-				UsageText: "stader-cli api network can-generate-rewards-tree index",
-				Action: func(c *cli.Context) error {
-
-					// Validate args
-					if err := cliutils.ValidateArgCount(c, 1); err != nil {
-						return err
-					}
-
-					index, err := cliutils.ValidateUint("index", c.Args().Get(0))
-					if err != nil {
-						return err
-					}
-
-					// Run
-					api.PrintResponse(canGenerateRewardsTree(c, index))
-					return nil
-
-				},
-			},
-
-			{
-				Name:      "generate-rewards-tree",
-				Usage:     "Set a request marker for the watchtower to generate the rewards tree for the given interval",
-				UsageText: "stader-cli api network generate-rewards-tree index",
-				Action: func(c *cli.Context) error {
-
-					// Validate args
-					if err := cliutils.ValidateArgCount(c, 1); err != nil {
-						return err
-					}
-
-					index, err := cliutils.ValidateUint("index", c.Args().Get(0))
-					if err != nil {
-						return err
-					}
-
-					// Run
-					api.PrintResponse(generateRewardsTree(c, index))
 					return nil
 
 				},
