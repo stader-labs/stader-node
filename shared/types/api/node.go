63ff4ba474830bf16304a5435d362a97f608445d
batphonghan <vanhungdeveloper@gmail.com>
2023-03-30 14:11:48 +0700

    Revert "Remove reward in response"
    This reverts commit 3fe7529184fcc448cc7ed457ccf87e4a35126519.

--- shared/types/api/node.go
+++ shared/types/api/node.go
@@ -21,6 +21,7 @@ type NodeStatusResponse struct {
 	OperatorId                   *big.Int             `json:"operatorId"`
 	OperatorName                 string               `json:"operatorName"`
 	OperatorRewardAddress        common.Address       `json:"operatorRewardAddress"`
+	OperatorRewardInETH          *big.Int             `json:"operatorRewardInETH"`
 	DepositedSdCollateral        *big.Int             `json:"depositedSdCollateral"`
 	SdCollateralWorthValidators  *big.Int             `json:"sdCollateralWorthValidators"`
 	Registered                   bool                 `json:"registered"`
