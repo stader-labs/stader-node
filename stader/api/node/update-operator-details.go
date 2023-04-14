49513b7d6d43bf6e430040a795386895c932e008
Bharath Vedartham <vedabharath12345@gmail.com>
2023-04-14 23:39:56 +0530

    clean up remaining logs

--- stader/api/node/update-operator-details.go
+++ stader/api/node/update-operator-details.go
@@ -75,13 +75,11 @@ func CanUpdateOperatorDetails(c *cli.Context, operatorName string, operatorRewar
 	}
 
 	// estimate gas
-	fmt.Println("Estimating gas...")
 	gasInfo, err := node.EstimateUpdateOperatorDetails(pnr, operatorName, operatorRewardAddress, opts)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println("Done with estimate gas. Returning response...")
 	response.GasInfo = gasInfo
 
 	return &response, nil
