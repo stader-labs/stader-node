c7e3edf087a681ced6634c0c6e775d634048c407
Bharath Vedartham <vedabharath12345@gmail.com>
2023-04-17 15:26:26 +0530

    add checks for deposit command

--- stader-cli/node/deposit.go
+++ stader-cli/node/deposit.go
@@ -92,6 +92,14 @@ func nodeDeposit(c *cli.Context) error {
 		fmt.Printf("Max validator limit reached")
 		return nil
 	}
+	if canNodeDepositResponse.OperatorNotRegistered {
+		fmt.Printf("Operator not registered")
+		return nil
+	}
+	if canNodeDepositResponse.OperatorNotActive {
+		fmt.Printf("Operator not active")
+		return nil
+	}
 
 	//Assign max fees
 	err = gas.AssignMaxFeeAndLimit(canNodeDepositResponse.GasInfo, staderClient, c.Bool("yes"))
