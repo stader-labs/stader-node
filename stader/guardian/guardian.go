b1bbf2cb1cb2232d2e666786eec5d934202ac39e
Bharath Vedartham <vedabharath12345@gmail.com>
2023-04-17 23:39:42 +0530

    wait for eth1 and eth2 clients to sync

--- stader/guardian/guardian.go
+++ stader/guardian/guardian.go
@@ -62,12 +62,24 @@ func RegisterCommands(app *cli.App, name string, aliases []string) {
 // Run daemon
 func run(c *cli.Context) error {
 
-	// Configure
-	configureHTTP()
-
 	// Initialize error logger
 	errorLog := log.NewColorLogger(ErrorColor)
 	updateLog := log.NewColorLogger(UpdateColor)
+
+	// wait for eth1 and eth2 clients to sync
+	err := services.WaitEthClientSynced(c, false) // Force refresh the primary / fallback EC status
+	if err != nil {
+		return err
+	}
+
+	// Check the BC status
+	err = services.WaitBeaconClientSynced(c, false) // Force refresh the primary / fallback BC status
+	if err != nil {
+		return err
+	}
+
+	// Configure
+	configureHTTP()
 
 	cfg, err := services.GetConfig(c)
 	if err != nil {
