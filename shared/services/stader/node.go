88a22f7657653978ba9589e6ddaaaa0dfdc074fa
Bharath Vedartham <vedabharath12345@gmail.com>
2023-04-26 01:07:12 +0530

    fix

--- shared/services/stader/node.go
+++ shared/services/stader/node.go
@@ -562,16 +562,16 @@ func (c *Client) CanDownloadSpMerkleProofs() (api.CanDownloadSpMerkleProofsRespo
 }
 
 func (c *Client) DownloadSpMerkleProofs() (api.DownloadSpMerkleProofsResponse, error) {
-	responseBytes, err := c.callAPI(fmt.Sprintf("node detailed-cycles-info"))
+	responseBytes, err := c.callAPI(fmt.Sprintf("node download-sp-merkle-proofs"))
 	if err != nil {
-		return api.DownloadSpMerkleProofsResponse{}, fmt.Errorf("could not get node detailed-cycles-info response: %w", err)
+		return api.DownloadSpMerkleProofsResponse{}, fmt.Errorf("could not get node download-sp-merkle-proofs response: %w", err)
 	}
 	var response api.DownloadSpMerkleProofsResponse
 	if err := json.Unmarshal(responseBytes, &response); err != nil {
-		return api.DownloadSpMerkleProofsResponse{}, fmt.Errorf("could not decode node detailed-cycles-info response: %w", err)
+		return api.DownloadSpMerkleProofsResponse{}, fmt.Errorf("could not decode node download-sp-merkle-proofs response: %w", err)
 	}
 	if response.Error != "" {
-		return api.DownloadSpMerkleProofsResponse{}, fmt.Errorf("could not get node detailed-cycles-info response: %s", response.Error)
+		return api.DownloadSpMerkleProofsResponse{}, fmt.Errorf("could not get node download-sp-merkle-proofs response: %s", response.Error)
 	}
 
 	return response, nil
