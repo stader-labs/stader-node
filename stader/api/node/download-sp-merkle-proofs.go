6d923fe10ce588c79d4b189d58e47a3bfedc30ba
Bharath Vedartham <vedabharath12345@gmail.com>
2023-04-26 01:16:46 +0530

    uncomment code

--- stader/api/node/download-sp-merkle-proofs.go
+++ stader/api/node/download-sp-merkle-proofs.go
@@ -7,67 +7,67 @@ import (
 	"github.com/stader-labs/stader-node/shared/services"
 	"github.com/stader-labs/stader-node/shared/types/api"
 	"github.com/stader-labs/stader-node/shared/utils/stader"
+	socializing_pool "github.com/stader-labs/stader-node/stader-lib/socializing-pool"
 	"github.com/urfave/cli"
+	"math/big"
 	"os"
 )
 
 func canDownloadSpMerkleProofs(c *cli.Context) (*api.CanDownloadSpMerkleProofsResponse, error) {
-	//if err := services.RequireNodeWallet(c); err != nil {
-	//	return nil, err
-	//}
-	//if err := services.RequireNodeRegistered(c); err != nil {
-	//	return nil, err
-	//}
-	//sp, err := services.GetSocializingPoolContract(c)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//cfg, err := services.GetConfig(c)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//response := api.CanDownloadSpMerkleProofsResponse{}
-	//
-	//// check if all cycles are present
-	//rewardDetails, err := socializing_pool.GetRewardDetails(sp, nil)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//currentIndex := rewardDetails.CurrentIndex.Int64()
-	//missingCycles := []int64{}
-	//// iterate thru all cycles starting from 1
-	//for i := int64(1); i < currentIndex; i++ {
-	//	isEligible, err := IsEligibleForCycle(c, big.NewInt(i))
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	if !isEligible {
-	//		continue
-	//	}
-	//
-	//	// download all cycles irrespective if the NO claim or not claimed.
-	//	cycleMerkleRewardFile := cfg.StaderNode.GetSpRewardCyclePath(i, true)
-	//	// check if file exists or not
-	//	_, err = os.Stat(cycleMerkleRewardFile)
-	//	if !os.IsNotExist(err) && err != nil {
-	//		return nil, err
-	//	}
-	//	if os.IsNotExist(err) {
-	//		missingCycles = append(missingCycles, i)
-	//	}
-	//}
-	//
-	//// no missing cycles
-	//if len(missingCycles) == 0 {
-	//	response.NoMissingCycles = true
-	//	return &response, nil
-	//}
+	if err := services.RequireNodeWallet(c); err != nil {
+		return nil, err
+	}
+	if err := services.RequireNodeRegistered(c); err != nil {
+		return nil, err
+	}
+	sp, err := services.GetSocializingPoolContract(c)
+	if err != nil {
+		return nil, err
+	}
+	cfg, err := services.GetConfig(c)
+	if err != nil {
+		return nil, err
+	}
 
 	response := api.CanDownloadSpMerkleProofsResponse{}
 
-	response.MissingCycles = []int64{1, 2, 3, 4, 5}
-	response.CurrentCycle = 5
+	// check if all cycles are present
+	rewardDetails, err := socializing_pool.GetRewardDetails(sp, nil)
+	if err != nil {
+		return nil, err
+	}
+	currentIndex := rewardDetails.CurrentIndex.Int64()
+	missingCycles := []int64{}
+	// iterate thru all cycles starting from 1
+	for i := int64(1); i <= currentIndex; i++ {
+		isEligible, err := IsEligibleForCycle(c, big.NewInt(i))
+		if err != nil {
+			return nil, err
+		}
+		if !isEligible {
+			continue
+		}
+
+		// download all cycles irrespective if the NO claim or not claimed.
+		cycleMerkleRewardFile := cfg.StaderNode.GetSpRewardCyclePath(i, true)
+		// check if file exists or not
+		_, err = os.Stat(cycleMerkleRewardFile)
+		if !os.IsNotExist(err) && err != nil {
+			return nil, err
+		}
+		if os.IsNotExist(err) {
+			missingCycles = append(missingCycles, i)
+		}
+	}
+
+	// no missing cycles
+	if len(missingCycles) == 0 {
+		response.NoMissingCycles = true
+		return &response, nil
+	}
+
+	response.MissingCycles = missingCycles
+	response.CurrentCycle = currentIndex
 
 	return &response, nil
 }
@@ -81,14 +81,6 @@ func downloadSpMerkleProofs(c *cli.Context) (*api.DownloadSpMerkleProofsResponse
 	if err != nil {
 		return nil, err
 	}
-	//sp, err := services.GetSocializingPoolContract(c)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//rewardDetails, err := socializing_pool.GetRewardDetails(sp, nil)
-	//if err != nil {
-	//	return nil, err
-	//}
 	nodeAccount, err := w.GetNodeAccount()
 	if err != nil {
 		return nil, err
