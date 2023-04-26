f8a2a8a0b2597633bbfea3df40c7852632778e5a
Bharath Vedartham <vedabharath12345@gmail.com>
2023-04-26 12:53:16 +0530

    only show remaining unclaimed rewards if rewards > 0

--- stader-cli/node/status.go
+++ stader-cli/node/status.go
@@ -154,8 +154,10 @@ func getStatus(c *cli.Context) error {
 		fmt.Printf("Operator Status: Not Active\n\n")
 	}
 
-	fmt.Printf("The Operator reward address %s has %.6f SD as unclaimed EL rewards through socializing pool till %s\n\n", status.OperatorAddress.String(), math.RoundDown(eth.WeiToEth(totalUnclaimedSocializingPoolSd), 18), status.SocializingPoolStartTime.String())
-	fmt.Printf("To claim SD rewards using the %sstader-cli node claim-sp-rewards%s command\n\n", log.ColorGreen, log.ColorReset)
+	if totalUnclaimedSocializingPoolSd.Cmp(big.NewInt(0)) > 0 {
+		fmt.Printf("The Operator reward address %s has %.6f SD as unclaimed EL rewards through socializing pool till %s\n\n", status.OperatorAddress.String(), math.RoundDown(eth.WeiToEth(totalUnclaimedSocializingPoolSd), 18), status.SocializingPoolStartTime.String())
+		fmt.Printf("To claim SD rewards using the %sstader-cli node claim-sp-rewards%s command\n\n", log.ColorGreen, log.ColorReset)
+	}
 
 	if !status.OptedInForSocializingPool {
 		fmt.Printf("Operator has Opted Out for Socializing Pool\n\n")
@@ -174,8 +176,11 @@ func getStatus(c *cli.Context) error {
 	} else {
 		fmt.Printf("Operator has Opted In for Socializing Pool\n\n")
 		fmt.Printf("Operator Socializing Pool Fee Recepient: %s\n\n", status.OperatorELRewardsAddress.String())
-		fmt.Printf("The Operator reward address %s has %.6f ETH as unclaimed EL rewards through socializing pool till %s\n\n", status.OperatorAddress.String(), math.RoundDown(eth.WeiToEth(totalUnclaimedSocializingPoolEth), 18), status.SocializingPoolStartTime.String())
-		fmt.Printf("To claim Socializing pool EL rewards using the %sstader-cli node claim-sp-rewards%s command\n\n", log.ColorGreen, log.ColorReset)
+
+		if totalUnclaimedSocializingPoolSd.Cmp(big.NewInt(0)) > 0 {
+			fmt.Printf("The Operator reward address %s has %.6f ETH as unclaimed EL rewards through socializing pool till %s\n\n", status.OperatorAddress.String(), math.RoundDown(eth.WeiToEth(totalUnclaimedSocializingPoolEth), 18), status.SocializingPoolStartTime.String())
+			fmt.Printf("To claim Socializing pool EL rewards using the %sstader-cli node claim-sp-rewards%s command\n\n", log.ColorGreen, log.ColorReset)
+		}
 	}
 
 	fmt.Printf("%s=== Registered Validator Details ===%s\n", log.ColorGreen, log.ColorReset)
