dc0243440969ce9815cee477f7064cfa0695b6aa
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-02 22:19:08 +0530

    update api links

--- shared/services/stader/external-urls.go
+++ shared/services/stader/external-urls.go
@@ -1,6 +1,6 @@
 package stader
 
-const PreSignSendApi = "https://1r6l0g1nkd.execute-api.us-east-1.amazonaws.com/prod/presign"
-const PreSignCheckApi = "https://1r6l0g1nkd.execute-api.us-east-1.amazonaws.com/prod/msgSubmitted"
-const PublicKeyApi = "https://1r6l0g1nkd.execute-api.us-east-1.amazonaws.com/prod/publicKey"
-const MerkleProofAggregateGetterApi = "https://1r6l0g1nkd.execute-api.us-east-1.amazonaws.com/prod/merklesForElRewards/proofs/%s"
+const PreSignSendApi = "https://stage-ethx-offchain.staderlabs.click/presign"
+const PreSignCheckApi = "https://stage-ethx-offchain.staderlabs.click/msgSubmitted"
+const PublicKeyApi = "https://stage-ethx-offchain.staderlabs.click/publicKey"
+const MerkleProofAggregateGetterApi = "https://stage-ethx-offchain.staderlabs.click/merklesForElRewards/proofs/%s"
