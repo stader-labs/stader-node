fc2e9a39beafb0debff48e046293080a398d51f4
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-24 00:57:58 +0530

    remove add on graffiti from vc start script

--- install/scripts/start-vc.sh
+++ install/scripts/start-vc.sh
@@ -94,12 +94,7 @@ if [ "$CC_CLIENT" = "lighthouse" ]; then
         CMD="$CMD --monitoring-endpoint $BITFLY_NODE_METRICS_ENDPOINT?apikey=$BITFLY_NODE_METRICS_SECRET&machine=$BITFLY_NODE_METRICS_MACHINE_NAME"
     fi
 
-    if [ "$ADDON_GWW_ENABLED" = "true" ]; then
-        echo "default: $GRAFFITI" > $GWW_GRAFFITI_FILE # Default graffiti value for Lighthouse
-        exec ${CMD} --graffiti-file $GWW_GRAFFITI_FILE
-    else
-        exec ${CMD} --graffiti "$GRAFFITI"
-    fi
+    exec ${CMD} --graffiti "$GRAFFITI"
 
 fi
 
@@ -225,12 +220,9 @@ if [ "$CC_CLIENT" = "prysm" ]; then
         CMD="$CMD --disable-account-metrics"
     fi
 
-    if [ "$ADDON_GWW_ENABLED" = "true" ]; then
-        echo "ordered:\n  - $GRAFFITI" > $GWW_GRAFFITI_FILE # Default graffiti value for Prysm
-        exec ${CMD} --graffiti-file=$GWW_GRAFFITI_FILE
-    else
-        exec ${CMD} --graffiti "$GRAFFITI"
-    fi
+
+    exec ${CMD} --graffiti "$GRAFFITI"
+
 
 fi
 
@@ -273,12 +265,7 @@ if [ "$CC_CLIENT" = "teku" ]; then
         CMD="$CMD --metrics-publish-endpoint=$BITFLY_NODE_METRICS_ENDPOINT?apikey=$BITFLY_NODE_METRICS_SECRET&machine=$BITFLY_NODE_METRICS_MACHINE_NAME"
     fi
 
-    if [ "$ADDON_GWW_ENABLED" = "true" ]; then
-        echo "$GRAFFITI" > $GWW_GRAFFITI_FILE # Default graffiti value for Teku
-        exec ${CMD} --validators-graffiti-file=$GWW_GRAFFITI_FILE
-    else
-        exec ${CMD} --validators-graffiti="$GRAFFITI"
-    fi
+    exec ${CMD} --validators-graffiti="$GRAFFITI"
 
 fi
 
