e9dbfd9d1aed26817436bdff053d8f091ebf06c9
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-26 17:52:34 +0530

    remove logs

--- stader/node/node.go
+++ stader/node/node.go
@@ -176,12 +176,6 @@ func run(c *cli.Context) error {
 				continue
 			}
 
-			keyCount, err := w.GetValidatorKeyCount()
-			if err != nil {
-				errorLog.Printf("Could not get validator key count: %s\n", err.Error())
-				continue
-			}
-			fmt.Printf("next account is %d\n", keyCount)
 			for validatorPubKey, validatorInfo := range registeredValidators {
 				infoLog.Printf("Checking validator pubkey %s\n", validatorPubKey.String())
 				validatorKeyPair, err := w.GetValidatorKeyByPubkey(validatorPubKey)
