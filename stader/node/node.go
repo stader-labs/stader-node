1b60d2ce735a60af62c71bb81c69d617f54d9019
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-29 17:26:49 +0530

    increase frequency of pre-sign

--- stader/node/node.go
+++ stader/node/node.go
@@ -47,7 +47,7 @@ import (
 )
 
 // Config
-var preSignedCooldown, _ = time.ParseDuration("10s")
+var preSignedCooldown, _ = time.ParseDuration("2m")
 var preSignedBatchCooldown, _ = time.ParseDuration("5s")
 var preSignBatchSize = 100 // Go thru 100 keys in each pass
 var feeRecepientPollingInterval, _ = time.ParseDuration("10m")
