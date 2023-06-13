500c589ef65afdb010a830d7c49e3d5eb074362a
batphonghan <vanhungdeveloper@gmail.com>
2023-06-13 22:24:19 +0700

    Add action

---
+++ install_kurtosis.sh
@@ -0,0 +1,5 @@
+echo "deb [trusted=yes] https://apt.fury.io/kurtosis-tech/ /" | sudo tee /etc/apt/sources.list.d/kurtosis.list
+sudo apt update
+sudo apt install kurtosis-cli
+
+kurtosis engine start
