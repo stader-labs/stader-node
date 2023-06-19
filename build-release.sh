be9242088a44f23c86c2aea158cba11ab83a8d70
Bharath Vedartham <vedabharath12345@gmail.com>
2023-06-19 18:51:19 +0530

    fix

--- build-release.sh
+++ build-release.sh
@@ -28,7 +28,7 @@
 # =================
 
 DOCKER_ACCOUNT=staderlabs
-S3_BUCKET=/eth/stader-node-build/permissionless
+S3_BUCKET=stader-cli-permissionless/eth/stader-node-build/permissionless
 
 # Print a failure message to stderr and exit
 fail() {
@@ -56,7 +56,7 @@ build_install_packages() {
     tar cfJ stader-node-install.tar.xz install || fail "Error building installer package."
     mv stader-node-install.tar.xz build/$VERSION
     cp install.sh build/$VERSION
-    aws s3 cp build/$VERSION $S3_BUCKET/$VERSION --recursive
+    aws s3 cp build/$VERSION s3://$S3_BUCKET/$VERSION --recursive
     echo "done!"
 
 }
