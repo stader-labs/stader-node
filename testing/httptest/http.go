081851ff15e4ebabe59820a4b81cac54fcf772db
batphonghan <vanhungdeveloper@gmail.com>
2023-06-22 12:44:23 +0700

    Generate key

--- testing/httptest/http.go
+++ testing/httptest/http.go
@@ -1,43 +1,107 @@
 package httptest
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"net/http"
 	"testing"
 
 	stader_backend "github.com/stader-labs/stader-node/shared/types/stader-backend"
+	"github.com/stader-labs/stader-node/shared/utils/crypto"
 	"github.com/stader-labs/stader-node/stader-lib/types"
 	"github.com/stretchr/testify/require"
 )
 
 type StaderHandler struct {
-	data map[string]interface{}
-	t    *testing.T
+	data   map[string]interface{}
+	t      *testing.T
+	pubPEM string
+	keyPEM []byte
+}
+
+func makeHanlde(t *testing.T) StaderHandler {
+	privatekey, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.Nil(t, err)
+
+	publickey := &privatekey.PublicKey
+
+	// Encode private key to PKCS#1 ASN.1 PEM.
+	keyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: x509.MarshalPKCS1PrivateKey(privatekey),
+		},
+	)
+
+	// Encode public key to PKCS#1 ASN.1 PEM.
+	pubPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: x509.MarshalPKCS1PublicKey(publickey),
+		},
+	)
+
+	pubkey := crypto.EncodeBase64(pubPEM)
+
+	s := StaderHandler{
+		data:   make(map[string]interface{}),
+		t:      t,
+		pubPEM: pubkey,
+		keyPEM: keyPEM,
+	}
+
+	return s
 }
 
 func SererHttp(t *testing.T) {
 	mux := http.NewServeMux()
-	s := StaderHandler{
-		data: make(map[string]interface{}),
-		t:    t,
-	}
-	mux.HandleFunc("/presigns", s.presigns)
-	mux.HandleFunc("/publicKey", s.publicKey)
+	s := makeHanlde(t)
 	mux.HandleFunc("/presign", s.presign)
+	mux.HandleFunc("/presigns", s.presigns)
+	mux.HandleFunc("/msgSubmitted", s.msgSubmitted)
 	mux.HandleFunc("/presignsSubmitted", s.presignsSubmitted)
+	mux.HandleFunc("/publicKey", s.publicKey)
+	mux.HandleFunc("/merklesForElRewards", s.merklesForElRewards)
 
 	err := http.ListenAndServe(":9989", mux)
 
 	require.Nil(t, err)
 }
 
-func (s *StaderHandler) presignsSubmitted(w http.ResponseWriter, r *http.Request) {
+func (s *StaderHandler) merklesForElRewards(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var validatorPubKeys map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&validatorPubKeys)
+
+	var p map[string]bool
+	// for _, v := range validatorPubKeys {
+	// 	p[v.String()] = false
+	// }
+	json.NewEncoder(w).Encode(p)
+}
+
+func (s *StaderHandler) msgSubmitted(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var validatorPubKeys []types.ValidatorPubkey
 	json.NewDecoder(r.Body).Decode(&validatorPubKeys)
 
 	var p map[string]bool
 	for _, v := range validatorPubKeys {
+		p[v.String()] = false
+	}
+	json.NewEncoder(w).Encode(p)
+}
+
+func (s *StaderHandler) presignsSubmitted(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var validatorPubKeys stader_backend.BulkPreSignCheckApiRequestType
+	json.NewDecoder(r.Body).Decode(&validatorPubKeys)
+
+	p := make(map[string]bool)
+	for _, v := range validatorPubKeys.ValidatorPubKeys {
 		p[v.String()] = false
 	}
 	json.NewEncoder(w).Encode(p)
@@ -53,8 +117,12 @@ func (s *StaderHandler) presign(w http.ResponseWriter, r *http.Request) {
 
 func (s *StaderHandler) publicKey(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	var validatorPubKeys map[string]interface{}
+	json.NewDecoder(r.Body).Decode(&validatorPubKeys)
+
 	p := stader_backend.PublicKeyApiResponse{
-		Value: "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUNDZ0tDQWdFQXJIUXhMQXVuY0J3blFmUlo0QTdneXlhTHZ5TjFjenMrbWJrTzdLZkx3L25SVXFITTZ6ak4Kd1pSbXM4SDFMQ0tCVnhUMWVBYVhWZ05RSlE2NmgvYnYxYS9GSHdzU1Z2eDRyWlZ1SXRTN0hVMWNFTm5YYXZpbwprcGYrbW9odXE1UC9zaDVRY3FKTjZJU3JhRmlyZHI1allNUXhIUE9SV0M5aEhZTjJxZE9RUmRCMmhxN0Y3UThwCmxIT0FDQlI4VTUvTm43WDMzWkpiWVdtekF2dE10K3JFMVBWWi9LczY5dm84Yyt4bTFQRFpsZVYxQTgzUGdEZDMKOURWMXZQSS9pZXE3OWRzQVQyQVZTVVpoVm5kSExhaXdvczZIcDNVUHlXbnpaQTVoNGRCblo5Nk53bGw4VCsrOApyZ3MrbldrQVU4dXg3dUhnWjlyeVJUR2NudnZubFdmUEpvUThDVDRCWVN6QURWMUphMnUvN3pXZ0VQZm1EK0kyCnQ5ejU1eGJpZis2QVhHYXY0dHRITUs3dzB1aU1nZnMrU1J6SU0xSDJXZklXeFVzVTBNQnFCYWIvMmdSb3k4dkMKWFE0ZVJFNWpvdkR5OVNpMUlzbzZYTTc5Vm1FWklseWFzS2NLUlRZMTRQd1dJVEFQek5xRThub01QLzJTcmhJNwpiWUhJY3d6Z0pGbk15b3g4QXM4T3p3SlMzY1lwZTk5cHVGblVoWmxrTjRIUjd5cEdOV0luRVZBcEl5aFVCMVd6CjlJcmpTNHR6THoyVUhZdTNKOFpMUmdlWERxSVdNbUlmWk5wazFETUV1eFo1eFFtUS9oR0toVVBlZGo5NVJ0a2cKUUtCeWN6QnRndHorSE11SUFJOHpFcUJ1WmNkWS9zakFnZjM5QVU1RXdhQ0JqSFJIS0NyR3A0RUNBd0VBQVE9PQotLS0tLUVORCBSU0EgUFVCTElDIEtFWS0tLS0t",
+		Value: s.pubPEM,
 	}
 	json.NewEncoder(w).Encode(p)
 }
