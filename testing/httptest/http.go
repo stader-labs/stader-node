a5a3463eedd8fe77f62eb0eb817c22200d607736
batphonghan <vanhungdeveloper@gmail.com>
2023-06-26 11:12:57 +0700

    Update resp type

--- testing/httptest/http.go
+++ testing/httptest/http.go
@@ -113,7 +113,10 @@ func (s *StaderHandler) presignsSubmitted(w http.ResponseWriter, r *http.Request
 }
 
 func (s *StaderHandler) presigns(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	var preSignedMessages []stader_backend.PreSignSendApiRequestType
+	var resp []stader_backend.PreSignSendApiResponseType
+
 	err := json.NewDecoder(r.Body).Decode(&preSignedMessages)
 	require.Nil(s.t, err)
 
@@ -123,9 +126,13 @@ func (s *StaderHandler) presigns(w http.ResponseWriter, r *http.Request) {
 		_, err = crypto.DecryptUsingPublicKey(decodeSig, s.privatekey)
 
 		require.Nil(s.t, err)
+		resp = append(resp, stader_backend.PreSignSendApiResponseType{
+			Success: true,
+			Error:   "",
+		})
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
 }
 
 func (s *StaderHandler) presign(w http.ResponseWriter, r *http.Request) {
