2905f9ba20d575bf1c38131b30478bab2e8b20f5
Bharath Vedartham <vedabharath12345@gmail.com>
2023-07-10 12:50:10 +0530

    avoid using mev portable builds

--- shared/services/config/mev-boost-config.go
+++ shared/services/config/mev-boost-config.go
@@ -29,7 +29,7 @@ import (
 
 // Constants
 const (
-	mevBoostPortableTag         string = "flashbots/mev-boost:1.6-portable"
+	mevBoostPortableTag         string = "flashbots/mev-boost:1.6"
 	mevBoostModernTag           string = "flashbots/mev-boost:1.6"
 	mevBoostUrlEnvVar           string = "MEV_BOOST_URL"
 	mevBoostRelaysEnvVar        string = "MEV_BOOST_RELAYS"
