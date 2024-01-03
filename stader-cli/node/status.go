b379a2a6d5ba3e7d3657e09987d25173ef622f28
Bharath Vedartham <vedabharath12345@gmail.com>
2024-01-03 17:55:43 +0530

    compare with 0

--- stader-cli/node/status.go
+++ stader-cli/node/status.go
@@ -118,7 +118,7 @@ func getNodeStatus(c *cli.Context) error {
 		fmt.Printf("2. Use the %sstader-cli node claim-rewards%s command to claim the EL rewards from the claim vault to your operator reward address\n\n", log.ColorGreen, log.ColorReset)
 	}
 
-	if status.OperatorRewardCollectorBalance.Cmp(big.NewInt(minEthBalanceForClaim)) > 0 {
+	if status.OperatorRewardCollectorBalance.Cmp(big.NewInt(0)) > 0 {
 		val, denom := eth.WeiToEthWithValCheck(status.OperatorRewardCollectorBalance)
 		if denom == "eth" {
 			fmt.Printf(
