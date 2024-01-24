c0674c7f82b3c72e12411e0679ca73eade653f66
Bharath Vedartham <vedabharath12345@gmail.com>
2024-01-24 12:13:19 +0530

    update version to 1.4.8-dev

--- shared/version.go
+++ shared/version.go
@@ -21,7 +21,7 @@ package shared
 
 const BinaryBucket string = "/stader-node-build/permissionless"
 const DockerAccount string = "staderlabs"
-const StaderVersion string = "1.4.7"
+const StaderVersion string = "1.4.8-dev"
 
 const Logo string = ` 
   _____ _            _             _           _       𝅺 	
