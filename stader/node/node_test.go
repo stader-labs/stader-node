2b9865d99fa4301ab0aa7871356ad7b0e750e402
batphonghan <vanhungdeveloper@gmail.com>
2024-02-02 14:14:27 +0700

    Sign message

---
+++ stader/node/node_test.go
@@ -0,0 +1,123 @@
+package node
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/crypto"
+	eCryto "github.com/ethereum/go-ethereum/crypto"
+	stader_backend "github.com/stader-labs/stader-node/shared/types/stader-backend"
+)
+
+var (
+	ExecutionClient = "Nethermind/v1.25.2+78c7bf5f/linux-x64/dotnet8.0.1"
+	ConsensusClient = "Lighthouse/v4.6.0-rc.0-2e8e160/x86_64-linux"
+)
+
+func TestVerifySignature(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(eCryto.S256(), rand.Reader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		t.Error(err)
+	}
+
+	publickeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	req, err := makesNodeDiversityRequest(&stader_backend.NodeDiversity{
+		ExecutionClient: ExecutionClient,
+		ConsensusClient: ConsensusClient,
+		NodeAddress:     crypto.PubkeyToAddress(*publicKeyECDSA).String(),
+		NodePublicKey:   hex.EncodeToString(publickeyBytes),
+	}, privateKey)
+	if err != nil {
+		t.Error(err)
+	}
+
+	msgStr := req.Message
+	verified := verifySignature(t, msgStr, req.Signature)
+
+	if !verified {
+		t.Error("verified should success")
+	}
+}
+
+func TestVerifySignatureFailed(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(eCryto.S256(), rand.Reader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		t.Error(err)
+	}
+
+	publickeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+
+	privateKeyFake, err := ecdsa.GenerateKey(eCryto.S256(), rand.Reader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	req, err := makesNodeDiversityRequest(&stader_backend.NodeDiversity{
+		ExecutionClient: ExecutionClient,
+		ConsensusClient: ConsensusClient,
+		NodeAddress:     crypto.PubkeyToAddress(*publicKeyECDSA).String(),
+		NodePublicKey:   hex.EncodeToString(publickeyBytes),
+	}, privateKeyFake)
+	if err != nil {
+		t.Error(err)
+	}
+
+	msgStr := req.Message
+	verified := verifySignature(t, msgStr, req.Signature)
+
+	if verified {
+		t.Error("verified should failed")
+	}
+}
+
+func verifySignature(t *testing.T, msgStr string, sig string) bool {
+	msgBytes, err := hex.DecodeString(msgStr)
+	if err != nil {
+		t.Error(err)
+	}
+
+	sign, err := hex.DecodeString(sig)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var msg stader_backend.NodeDiversity
+
+	err = json.Unmarshal(msgBytes, &msg)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if msg.ConsensusClient != ConsensusClient {
+		t.Error(err)
+	}
+
+	if msg.ExecutionClient != ExecutionClient {
+		t.Error(err)
+	}
+
+	messageHash := accounts.TextHash(msgBytes)
+
+	decodePubkey, err := hex.DecodeString(msg.NodePublicKey)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return eCryto.VerifySignature(decodePubkey, messageHash, sign)
+}
