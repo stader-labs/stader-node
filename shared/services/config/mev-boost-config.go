c5836c2f5834b945644a627cb1e3faa2fca4f976
Victor <victorelec14@gmail.com>
2024-02-27 20:40:20 +0100

    Bump flashbots/mev-boost to v1.7

--- shared/services/config/mev-boost-config.go
+++ shared/services/config/mev-boost-config.go
@@ -29,8 +29,8 @@ import (
 
 // Constants
 const (
-	mevBoostPortableTag         string = "flashbots/mev-boost:1.6"
-	mevBoostModernTag           string = "flashbots/mev-boost:1.6"
+	mevBoostPortableTag         string = "flashbots/mev-boost:1.7"
+	mevBoostModernTag           string = "flashbots/mev-boost:1.7"
 	mevBoostUrlEnvVar           string = "MEV_BOOST_URL"
 	mevBoostRelaysEnvVar        string = "MEV_BOOST_RELAYS"
 	mevDocsUrl                  string = "#"
