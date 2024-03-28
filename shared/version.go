ba7505fee9e00018efb613bcc8a89b750e6881ec
batphonghan <vanhungdeveloper@gmail.com>
2024-03-28 13:21:46 +0700

    Bump ver

--- shared/version.go
+++ shared/version.go
@@ -21,7 +21,7 @@ package shared
 
 const BinaryBucket string = "/stader-node-build/permissionless"
 const DockerAccount string = "staderdev"
-const StaderVersion string = "1.5.2-beta-1"
+const StaderVersion string = "1.5.2-beta-2"
 
 const Logo string = ` 
   _____ _            _             _           _       𝅺 	
