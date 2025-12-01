2f0be25fce68cc4e1d1ff7736ff5c0b9eaba249f
batphonghan <vanhungdeveloper@gmail.com>
2025-12-01 13:36:15 +0700

    Fusaka mainet

--- shared/services/config/nethermind-params.go
+++ shared/services/config/nethermind-params.go
@@ -28,8 +28,8 @@ import (
 
 // Constants
 const (
-	nethermindTagProd          string = "nethermind/nethermind:1.35.2"
-	nethermindTagTest          string = "nethermind/nethermind:1.35.2"
+	nethermindTagProd          string = "nethermind/nethermind:1.35.3"
+	nethermindTagTest          string = "nethermind/nethermind:1.35.3"
 	nethermindEventLogInterval int    = 1000
 	nethermindStopSignal       string = "SIGTERM"
 )
