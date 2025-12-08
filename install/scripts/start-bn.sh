6733b5a746785c56dc8b03b62e493c66995de6b5
degenSumi <sb152377@gmail.com>
2025-12-08 19:31:59 +0530

    chore: v172

--- install/scripts/start-bn.sh
+++ install/scripts/start-bn.sh
@@ -227,9 +227,6 @@ if [ "$CC_CLIENT" = "prysm" ]; then
         --enable-experimental-backfill \
         --blob-storage-layout=by-epoch \
         --disable-last-epoch-targets  \
-        --slots-per-archive-point=1048576 \
-        --force-clear-db  \
-        --verbosity=debug \
         $BN_ADDITIONAL_FLAGS"
 
     if [ ! -z "$MEV_BOOST_URL" ]; then
